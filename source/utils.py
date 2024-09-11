fa831951f35e7c02a2e62bdb389774ba4c940dde
EniyanEzhilan <eniyanezhilan101@gmail.com>
2024-09-11 00:37:20 -0400

    p

--- source/utils.py
+++ source/utils.py
@@ -25,21 +25,21 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(x_train, y_train,X_test,y_test,models):
+def evaluate_models(x_train, y_train,X_test,y_test,models,params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            #para=param[list(models.keys())[i]]
+            para=params[list(models.keys())[i]]
 
-            '''gs = GridSearchCV(model,para,cv=3)
-            gs.fit(X_train,y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)'''
+            model.fit(x_train,y_train)
 
-            model.fit(x_train, y_train)  # Train model
+            #model.fit(x_train, y_train)  # Train model
 
             y_train_pred = model.predict(x_train)
 
